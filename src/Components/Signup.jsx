94fca1f781bd93ccbd364de3808162d5b738ab8c
Zannatul Hema <shanzidadisha1234@gmail.com>
2024-08-05 04:51:28 +0600

    signup page fix

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -22,7 +22,7 @@ export default function Signup() {
   const navigate = useNavigate();
 
   // select branch option
-  const options = [
+  const branchoptions = [
     "Uk",
     "USA",
     "Canada",
@@ -100,77 +100,88 @@ export default function Signup() {
             expenses..
           </p>
         </div>
-        <form onSubmit={HandleSignup}>
+        <form onSubmit={HandleSignup} className="">
           {/* name */}
           <div className="grid grid-cols-2">
-            <label className="p-2 py-3 rounded-none rounded-tl outline-none flex items-center gap-2 hover:bg-gray-100 border">
-              <IoPersonOutline className="text-2xl  text-gray-300  " />
+            <div className="relative ">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <IoPersonOutline className="text-sm  text-gray-400  " />
+              </div>
               <input
                 name="firstName"
                 required
                 type="text"
-                className="grow text-sm  hover:bg-gray-100  outline-0 text-gray-400"
+                className="hover:bg-gray-100 border rounded-none outline-0 border-gray-200 rounded-tl  text-sm block w-full ps-10 p-2.5 text-gray-400 focus:outline-none"
                 placeholder="John"
-                o
               />
-            </label>
-            <label className="p-2 py-3 rounded-tr rounded-none outline-none flex items-center gap-2 hover:bg-gray-100 border border-l-0">
-              <IoPersonOutline className="text-2xl  text-gray-300 " />
+            </div>
+
+            <div className="relative ">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <IoPersonOutline className="text-sm  text-gray-400  " />
+              </div>
               <input
                 name="lastName"
                 required
                 type="text"
-                className="grow text-sm  hover:bg-gray-100  outline-0 text-gray-400"
+                className="hover:bg-gray-100 border rounded-none outline-0 border-gray-200 rounded-tr  text-sm block w-full ps-10 p-2.5 text-gray-400  border-l-0 focus:outline-none"
                 placeholder="Doe"
               />
-            </label>
+            </div>
           </div>
           {/* email */}
-          <label className="p-2 py-3 rounded-none outline-none flex items-center gap-2 hover:bg-gray-100 border border-t-0">
-            <GoMail className=" text-gray-300 " />
+          <div className="relative ">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <GoMail className=" text-gray-400 text-sm " />
+            </div>
             <input
               name="email"
               required
               type="email"
-              className="grow text-sm  hover:bg-gray-100  outline-0 text-gray-400"
+              className="hover:bg-gray-100 border rounded-none outline-0 border-gray-200 rounded-tr  text-sm block w-full ps-10 p-2.5 text-gray-400  border-y-0 focus:outline-none"
               placeholder="example@gmail.com"
             />
-          </label>
+          </div>
           {/* branch */}
-          <label className="w-full p-2 py-3 rounded-none outline-none flex items-center gap-2 hover:bg-gray-100 border border-t-0">
-            <CiLocationOn className=" text-gray-300 " />
+          <div className="relative ">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <CiLocationOn className="text-gray-400" />
+            </div>
             <select
-              name="branch"
               required
-              className="grow text-sm hover:bg-gray-100  outline-0 text-gray-400 focus:bg-transparent"
+              name="branch"
+              id="branch"
+              defaultValue={""}
+              className="hover:bg-gray-100  rounded-none outline-0 border-gray-200 text-sm block w-full ps-10 p-2.5 text-gray-400  border h-full   "
             >
-              <option disabled selected>
+              <option value="" disabled>
                 Select your branch
               </option>
-
-              {options?.map((option, index) => (
-                <option key={index} className="text-black">
+              {branchoptions?.map((option, i) => (
+                <option key={i} className="text-black ">
                   {option}
                 </option>
               ))}
             </select>
-          </label>
+          </div>
           {/* pass & confirm pass*/}
           <div className="grid grid-cols-2">
-            <label className="relative p-2 py-3 rounded-none rounded-bl hover:bg-gray-100 flex items-center gap-2  border-t-0 border">
-              <GoUnlock className=" text-gray-300" />
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <GoUnlock className=" text-gray-400 text-sm " />
+              </div>
               <input
                 name="password"
                 required
                 type={passwordVisible ? "text" : "password"}
-                className="grow text-sm hover:bg-gray-100  outline-0 focus:bg-transparent text-gray-400"
+                className="hover:bg-gray-100 border rounded-none outline-0 border-gray-200 rounded-tr  text-sm block w-full ps-10 p-2.5 text-gray-400  border-t-0 border-r-0 rounded-bl focus:outline-none"
                 placeholder="••••••••"
               />
               {/* Show/Hide Button */}
               <button
                 type="button"
                 onClick={() => setPasswordVisible(!passwordVisible)}
-                className="absolute right-3"
+                className="absolute right-3 top-0 bottom-0 my-auto"
               >
                 {passwordVisible ? (
                   <GoEyeClosed className="text-gray-300" />
@@ -178,14 +189,16 @@ export default function Signup() {
                   <GoEye className="text-gray-300" />
                 )}
               </button>
-            </label>
-            <label className="relative p-2 py-3 rounded-none rounded-br  hover:bg-gray-100 flex items-center gap-2  border-t-0 border-l-0 border">
-              <GoUnlock className=" text-gray-300" />
+            </div>
+            <div className="relative ">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <GoUnlock className=" text-gray-400 text-sm " />
+              </div>
               <input
                 name="confirmpass"
                 required
                 type={confirmPasswordVisible ? "text" : "password"}
-                className="grow text-sm hover:bg-gray-100 outline-0 text-gray-400"
+                className="hover:bg-gray-100 border rounded-none outline-0 border-gray-200 rounded-br  text-sm block w-full ps-10 p-2.5 text-gray-400  border-t-0 focus:outline-none"
                 placeholder="••••••••"
               />
               {/* Show/Hide Button */}
@@ -194,15 +207,16 @@ export default function Signup() {
                 onClick={() =>
                   setConfirmPasswordVisible(!confirmPasswordVisible)
                 }
-                className="absolute right-3 group"
+                className="absolute right-3 top-0 bottom-0 my-auto"
               >
-                {passwordVisible ? (
-                  <GoEyeClosed className="text-gray-300 " />
+                {confirmPasswordVisible ? (
+                  <GoEyeClosed className="text-gray-300" />
                 ) : (
                   <GoEye className="text-gray-300" />
                 )}
               </button>
-            </label>
+            </div>
+
             {!isPassSame && (
               <span className="text-sm text-primary-color font-medium my-1 flex gap-1 items-center">
                 <CiWarning /> <span>Password didn't match</span>
@@ -213,7 +227,7 @@ export default function Signup() {
           <div className="form-control mt-3 ">
             <label className="label cursor-pointer justify-start items-start gap-3 text-start ">
               <input
-              required
+                required
                 type="checkbox"
                 defaultChecked
                 className="checkbox checkbox-sm  rounded [--chkbg:theme(colors.primary-color)] [--chkfg:white] checked:border-0"
@@ -226,7 +240,7 @@ export default function Signup() {
           </div>
 
           {/* button */}
-          <div className="flex mt-4 items-center gap-2">
+          <div className="flex mt-4 items-center  gap-2">
             <Button
               type="submit"
               className="rounded-full bg-primary-color border border-primary-color font-medium hover:border-primary-color hover:bg-white hover:text-primary-color duration-400 hover:shadow-none"
