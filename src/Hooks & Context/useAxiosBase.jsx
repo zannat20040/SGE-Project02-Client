afeb774cb257eb53dcae248bff1987f2f32cd13e
Zannatul Hema <shanzidadisha1234@gmail.com>
2024-08-07 16:47:07 +0600

    api added

--- src/Hooks & Context/useAxiosBase.jsx
+++ src/Hooks & Context/useAxiosBase.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://sge-project02-backend.vercel.app",
 });
 
 const useAxiosBase = () => {
