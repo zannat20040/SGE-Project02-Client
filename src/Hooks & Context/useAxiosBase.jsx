f2110b1761fdff18938b2a7f0701ba115fd779af
zannat20040 <shanzidadisha1234@gmail.com>
2024-09-22 01:40:31 +0600

    clean code

--- src/Hooks & Context/useAxiosBase.jsx
+++ src/Hooks & Context/useAxiosBase.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  // baseURL: "http://localhost:5000",
-  baseURL: "https://sge-project02-backend.vercel.app",
+  baseURL: "http://localhost:5000",
+  // baseURL: "https://api-finance.studyuk.today",
+  // baseURL: "https://sge-project02-backend.vercel.app",
 });
 
 const useAxiosBase = () => {
