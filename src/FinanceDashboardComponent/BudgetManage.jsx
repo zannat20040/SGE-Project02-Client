2a5022f59e5c1869d89587eb2d12e0405b67bce2
zannat20040 <shanzidadisha1234@gmail.com>
2024-10-03 18:48:29 +0600

    change budget management ui

--- src/FinanceDashboardComponent/BudgetManage.jsx
+++ src/FinanceDashboardComponent/BudgetManage.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import BreadcrumsLayout from "../Shared Component/BreadcrumsLayout";
 import useUserInfo from "../Hooks & Context/useUserInfo";
 import { Radio } from "@material-tailwind/react";
+import BudgetRequest from "./BudgetRequest";
+import AllocateBudget from "./AllocateBudget";
 
 export default function BudgetManage() {
   const { userinfo } = useUserInfo();
@@ -15,28 +17,36 @@ export default function BudgetManage() {
         activeroute2={"budget-manage"}
       />
 
-      <div className="bg-white px-6 py-10 mt-3">
-        <div className="flex  justify-between gap-4 items-center">
-          <p className="p-5 font-semibold text-xl text-gray-500 ">
-           {selectedRadio=== 'allocate' ? 'Allocate the Budget' : 'Request for the Budget Expension'} 
-          </p>
-          <div className="flex gap-2 text-sm">
-            <Radio
-              name="type"
-              label="Budget Allocation"
-              checked={selectedRadio === "allocate"}
-              onChange={() => setSelectedRadio("allocate")}
-              color="amber"
-            />
-            <Radio
-              name="type"
-              label="Request for Budget"
-              checked={selectedRadio === "request"}
-              onChange={() => setSelectedRadio("request")}
-              color="amber"
-            />
+      <div className="grid grid-cols-5 gap-3 mt-3">
+        <div className="bg-white px-6 py-10 col-span-3">
+          <div className="flex  justify-between gap-5 items-center">
+            <p className="font-semibold text-xl text-primary-color ">
+              {selectedRadio === "allocate"
+                ? "Allocate the Budget"
+                : "Request for the Budget Expension"}
+            </p>
+            <div className="flex gap-2 text-sm">
+              <Radio
+                name="type"
+                label="Budget Allocation"
+                checked={selectedRadio === "allocate"}
+                onChange={() => setSelectedRadio("allocate")}
+                color="amber"
+              />
+              <Radio
+                name="type"
+                label="Request for Budget"
+                checked={selectedRadio === "request"}
+                onChange={() => setSelectedRadio("request")}
+                color="amber"
+              />
+            </div>
+          </div>
+          <div className="mt-20">
+            {selectedRadio==='allocate' ? <AllocateBudget /> : <BudgetRequest />}
           </div>
         </div>
+        <div className="bg-white px-6 py-10 col-span-2">dsviusdhvs</div>
       </div>
     </div>
   );
