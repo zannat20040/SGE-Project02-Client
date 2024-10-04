3adf5beeb854a2ef8f28e4665f7aa7d5b969de14
zannat20040 <shanzidadisha1234@gmail.com>
2024-10-04 19:33:14 +0600

    employee details get

--- src/FinanceDashboardComponent/AllocateBudget.jsx
+++ src/FinanceDashboardComponent/AllocateBudget.jsx
@@ -13,6 +13,7 @@ import { AuthContext } from "../AuthProvider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 import { MdAttachMoney } from "react-icons/md";
 import toast from "react-hot-toast";
+import ButtonLoading from "../Shared Component/ButtonLoading";
 
 export default function AllocateBudget() {
   const [open, setOpen] = React.useState(1);
@@ -21,15 +22,36 @@ export default function AllocateBudget() {
   const { user } = useContext(AuthContext);
   const [message, setMessage] = useState({ id: null, error: "" }); // Proper initialization
 
+  // const {
+  //   data: allBudgets,
+  //   isLoading,
+  //   refetch,
+  // } = useQuery({
+  //   queryKey: ["allBudgets", user?.email, userinfo?.branch],
+  //   queryFn: async () => {
+  //     const response = await axiosBase.get(
+  //       `finance/budgets?branch=${userinfo?.branch}&additionalBranch=all`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${user?.email}`,
+  //         },
+  //       }
+  //     );
+
+  //     const data = response?.data?.users || [];
+  //     return data;
+  //   },
+  // });
+
   const {
-    data: allBudgets,
+    data: employeeDetails,
     isLoading,
     refetch,
   } = useQuery({
-    queryKey: ["allBudgets", user?.email, userinfo?.branch],
+    queryKey: ["employeeDetails", user?.email, userinfo?.branch],
     queryFn: async () => {
       const response = await axiosBase.get(
-        `finance/budgets?branch=${userinfo?.branch}&additionalBranch=all`,
+        `/finance/getUsers?branch=${userinfo?.branch}&additional=all&role=employee,ceo,admin`,
         {
           headers: {
             Authorization: `Bearer ${user?.email}`,
@@ -41,6 +63,8 @@ export default function AllocateBudget() {
       return data;
     },
   });
+
+  console.log(employeeDetails);
 
   const HandleBudgetAllocate = async (e, id) => {
     e.preventDefault();
@@ -84,109 +108,120 @@ export default function AllocateBudget() {
 
   return (
     <div>
-      {allBudgets?.map((employee, index) => (
-        <Accordion open={open === index + 1} key={employee?._id} className="shadow p-5 pb-0">
-          {/* header */}
-          <AccordionHeader
-            onClick={() => handleOpen(index + 1)}
-            className="flex justify-between items-center gap-10 border-0"
+      {isLoading ? (
+        <div className="flex justify-center p-4">
+          <ButtonLoading />
+        </div>
+      ) : (employeeDetails && employeeDetails.length <= 0) ||
+        employeeDetails == undefined ? (
+        <p className="text-center p-4"> No finance available for this branch</p>
+      ) : (
+        employeeDetails.map((employee, index) => (
+          <Accordion
+            open={open === index + 1}
+            key={employee?._id}
+            className="shadow p-5 pb-0"
           >
-            <div className="flex flex-col items-start text-start w-full">
-              <p className="font-normal capitalize text-base">
-                {employee?.name}
-              </p>
-              <p className="text-xs font-medium text-gray-500 ">
-                {employee?.email}
-              </p>
-            </div>
-            <p className="text-right text-base font-semibold text-primary-color">
-              +{employee?.givenBudget}
-            </p>
-          </AccordionHeader>
-          {/* progress */}
-          <Progress
-            value={(
-              ((employee?.givenBudget - employee?.remainingBudget) /
-                employee?.givenBudget) *
-              100
-            ).toFixed(2)}
-            size="sm"
-            color={
-              ((employee?.givenBudget - employee?.remainingBudget) /
-                employee?.givenBudget) *
-                100 >=
-              70
-                ? "red"
-                : ((employee?.givenBudget - employee?.remainingBudget) /
-                    employee?.givenBudget) *
-                    100 >=
-                  40
-                ? "amber"
-                : "green"
-            }
-          />
-          {/* body */}
-          <AccordionBody>
-            <div>
-              <p className="font-normal">
-                Budget Allocated On :{" "}
-                <span className="font-semibold text-gray-500">
-                  {employee?.allocationDate.split("T")[0]}
-                </span>
-              </p>
-              <p className="font-normal">
-                Allocation Due to :{" "}
-                <span className="font-semibold text-gray-500">
-                  {employee?.dueDate
-                    ? employee?.dueDate?.split("T")[0]
-                    : "Not allocated"}
-                </span>
-              </p>
-            </div>
-
-            <div>
-              {/* Display error message for this specific employee */}
-              {message.id === employee?._id && (
-                <p className="font-semibold text-red-600 mt-6">
-                  {message.error}
+            {/* header */}
+            <AccordionHeader
+              onClick={() => handleOpen(index + 1)}
+              className="flex justify-between items-center gap-10 border-0"
+            >
+              <div className="flex flex-col items-start text-start w-full">
+                <p className="font-normal capitalize text-base">
+                  {employee?.firstName + " " + employee?.lastName}
                 </p>
-              )}
-              {/* form */}
-              <form
-                onSubmit={(e) => HandleBudgetAllocate(e, employee?._id)}
-                className="flex justify-between bg-white mt-6 border-2 rounded-full "
-              >
-                <div className="relative w-full">
-                  <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none ">
-                    <MdAttachMoney className="text-gray-400" />
-                  </div>
-                  <input
-                    disabled={employee?.dueDate}
-                    name="amount"
-                    required
-                    type="text"
-                    className=" rounded-l-full bg-white hover:bg-gray-100 rounded-none outline-none  text-sm block w-full ps-10 p-2.5 text-gray-800   h-full "
-                    placeholder={
-                      employee?.dueDate
-                        ? `You can allocate budget after ${
-                            employee?.dueDate?.split("T")[0]
-                          }`
-                        : "Enter allocated budget amount"
-                    }
-                  />
-                </div>
-                <Button
-                  disabled={employee?.dueDate}
-                  type="submit"
-                  className={`rounded-full bg-primary-color border rounded-l-none border-primary-color font-medium hover:border-primary-color hover:bg-white hover:text-primary-color duration-400 hover:shadow-none w-1/5`}
+                <p className="text-xs font-medium text-gray-500 ">
+                  {employee?.email}
+                </p>
+              </div>
+              <p className="text-right text-base font-semibold text-primary-color">
+                +{employee?.givenBudget || "0.00"}
+              </p>
+            </AccordionHeader>
+            {/* progress */}
+            <Progress
+              value={(
+                ((employee?.givenBudget - employee?.remainingBudget) /
+                  employee?.givenBudget) *
+                100
+              ).toFixed(2)}
+              size="sm"
+              color={
+                ((employee?.givenBudget - employee?.remainingBudget) /
+                  employee?.givenBudget) *
+                  100 >=
+                70
+                  ? "red"
+                  : ((employee?.givenBudget - employee?.remainingBudget) /
+                      employee?.givenBudget) *
+                      100 >=
+                    40
+                  ? "amber"
+                  : "green"
+              }
+            />
+            {/* body */}
+            <AccordionBody>
+              <div>
+                <p className="font-normal">
+                  Budget Allocated On :{" "}
+                  <span className="font-semibold text-gray-500">
+                    {employee?.allocationDate?.split("T")[0] || "Not allocated"}
+                  </span>
+                </p>
+                <p className="font-normal">
+                  Allocation Due to :{" "}
+                  <span className="font-semibold text-gray-500">
+                    {employee?.dueDate?.split("T")[0] || "Not allocated"}
+                  </span>
+                </p>
+              </div>
+
+              <div>
+                {/* Display error message for this specific employee */}
+                {message.id === employee?._id && (
+                  <p className="font-semibold text-red-600 mt-6">
+                    {message.error}
+                  </p>
+                )}
+                {/* form */}
+                <form
+                  onSubmit={(e) => HandleBudgetAllocate(e, employee?._id)}
+                  className="flex justify-between bg-white mt-6 border-2 rounded-full "
                 >
-                  Add this
-                </Button>
-              </form>
-            </div>
-          </AccordionBody>
-        </Accordion>
-      ))}
+                  <div className="relative w-full">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none ">
+                      <MdAttachMoney className="text-gray-400" />
+                    </div>
+                    <input
+                      disabled={employee?.dueDate}
+                      name="amount"
+                      required
+                      type="text"
+                      className=" rounded-l-full bg-white hover:bg-gray-100 rounded-none outline-none  text-sm block w-full ps-10 p-2.5 text-gray-800   h-full "
+                      placeholder={
+                        employee?.dueDate
+                          ? `You can allocate budget after ${
+                              employee?.dueDate?.split("T")[0]
+                            }`
+                          : "Enter allocated budget amount"
+                      }
+                    />
+                  </div>
+                  <Button
+                    disabled={employee?.dueDate}
+                    type="submit"
+                    className={`rounded-full bg-primary-color border rounded-l-none border-primary-color font-medium hover:border-primary-color hover:bg-white hover:text-primary-color duration-400 hover:shadow-none w-1/5`}
+                  >
+                    Add this
+                  </Button>
+                </form>
+              </div>
+            </AccordionBody>
+          </Accordion>
+        ))
+      )}
     </div>
   );
 }
